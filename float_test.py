34ad76c8b8cc8f0838e93f5a1515364c70eb4fa6
Hrvoje Abraham <ahrvoje@gmail.com>
2016-05-01 15:46:27 +0200

    strtod.wl to StringToDouble.wl, formatting tests include values

--- float_test.py
+++ float_test.py
@@ -1,21 +1,32 @@
 import struct
 import toml
 
+
 with open('strtod_tests.toml') as tests_file:
+    def execute_tests(test_class):
+        all_test_ok = True
+        for test in tests[test_class]:
+            test_str = test['str']
+
+            if 'nan' in test_str.lower():
+                continue
+
+            x = float(test_str)
+            hex_result = hex(struct.unpack('<Q', struct.pack('<d', x))[0])[2:].zfill(16)
+
+            if hex_result != test['hex']:
+                print('Test UID %s failed!' % test['UID'])
+                all_test_ok = False
+
+        if all_test_ok:
+            print('All %d %s passed OK.' % (len(tests[test_class]), test_class))
+
     tests = toml.loads(tests_file.read())
-    conversion_tests = tests['conversionTests']
 
-    all_test_ok = True
-    for conversion_test in conversion_tests:
-        x = float(conversion_test['str'])
-        hex_result = hex(struct.unpack('<Q', struct.pack('<d', x))[0])[2:].zfill(16)
+    execute_tests('FormattingTests')
+    execute_tests('ConversionTests')
 
-        if hex_result != conversion_test['hex']:
-            print('Test UID %s failed!' % conversion_test['UID'])
-            all_test_ok = False
-
-    if all_test_ok:
-        print('All %d conversion tests passed OK.' % len(conversion_tests))
 
 # Python 3.5.1 64-bit on Win10
-# All 75 conversion tests passed OK.
+# All 35 FormattingTests passed OK.
+# All 75 ConversionTests passed OK.
